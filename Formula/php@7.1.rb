1f2b92eac68f77f9eb37b37efee6ca602526eb7c
Oleg Ivanov <o.debretseni@tenantcloud.com>
2019-12-25 13:26:06 +0200

    fix php@7.1_openssl

--- Formula/php@7.1.rb
+++ Formula/php@7.1.rb
@@ -146,7 +146,7 @@ class PhpAT71 < Formula
       --with-mysql-sock=/tmp/mysql.sock
       --with-mysqli=mysqlnd
       --with-ndbm#{headers_path}
-      --with-openssl=#{Formula["openssl"].opt_prefix}
+      --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
       --with-pdo-dblib=#{Formula["freetds"].opt_prefix}
       --with-pdo-mysql=mysqlnd
       --with-pdo-odbc=unixODBC,#{Formula["unixodbc"].opt_prefix}
