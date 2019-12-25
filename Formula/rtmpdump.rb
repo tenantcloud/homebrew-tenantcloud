39aac537f308dfe9c4d2c422902f2c628db31458
Oleg Ivanov <o.debretseni@tenantcloud.com>
2019-12-25 16:32:04 +0200

    add rtmpdump_formula

---
+++ Formula/rtmpdump.rb
@@ -0,0 +1,41 @@
+class Rtmpdump < Formula
+  desc "Tool for downloading RTMP streaming media"
+  homepage "https://rtmpdump.mplayerhq.hu/"
+  url "http://10.10.4.242:8081/rtmpdump_2.4+20151223.gitfa8646d.1.orig.tar.gz"
+  version "2.4+20151223"
+  sha256 "5c032f5c8cc2937eb55a81a94effdfed3b0a0304b6376147b86f951e225e3ab5"
+  revision 1
+  head "https://git.ffmpeg.org/rtmpdump.git", :shallow => false
+
+  bottle do
+    cellar :any
+    sha256 "f39d714005d28ed61728832877433a68dd256796bc225bac68b505b2c1d97ef4" => :catalina
+  end
+
+  depends_on "openssl@1.1"
+
+  conflicts_with "flvstreamer", :because => "both install 'rtmpsrv', 'rtmpsuck' and 'streams' binary"
+
+  # Patch for OpenSSL 1.1 compatibility
+  # Taken from https://github.com/JudgeZarbi/RTMPDump-OpenSSL-1.1
+  patch :p0 do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/rtmpdump/openssl-1.1.diff"
+    sha256 "3c9167e642faa9a72c1789e7e0fb1ff66adb11d721da4bd92e648cb206c4a2bd"
+  end
+
+  def install
+    ENV.deparallelize
+    system "make", "CC=#{ENV.cc}",
+                   "XCFLAGS=#{ENV.cflags}",
+                   "XLDFLAGS=#{ENV.ldflags}",
+                   "MANDIR=#{man}",
+                   "SYS=darwin",
+                   "prefix=#{prefix}",
+                   "sbindir=#{bin}",
+                   "install"
+  end
+
+  test do
+    system "#{bin}/rtmpdump", "-h"
+  end
+end
