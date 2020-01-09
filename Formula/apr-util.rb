8a8296cc1c78e342524f7ffac6be490afbd0cdc4
Oleg Ivanov <o.debretseni@tenantcloud.com>
2020-01-09 12:49:46 +0200

    fix apr-util

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -1,14 +1,14 @@
 class AprUtil < Formula
   desc "Companion library to apr, the Apache Portable Runtime library"
   homepage "https://apr.apache.org/"
-   url "http://10.10.4.242:8081/apr-util-1.6.1.tar.gz"
+   url "http://10.10.4.242:8081/apr-util-1.6.1.tar.bz2"
 
   sha256 "d3e12f7b6ad12687572a3a39475545a072608f4ba03a6ce8a3778f607dd0035b"
   revision 3
 
   bottle do
     root_url "http://10.10.4.242:8081/bottles"
-    sha256 "e4927892e16a3c9cf0d037c1777a6e5728fef2f5abfbc0af3d0d444e9d6a1d2b" => :mojave
+    sha256 "b3b8376d8f481164a34b891b926ab22acdc2903e77c4cfbc04c0ba6363ca7597" => :mojave
     sha256 "1bdf0cda4f0015318994a162971505f9807cb0589a4b0cbc7828531e19b6f739" => :high_sierra
     sha256 "425955a21c3fec8e78f365cd7fc4c6c4ec95d074f720a9b24e8237af90cc4dcc" => :catalina
   end
