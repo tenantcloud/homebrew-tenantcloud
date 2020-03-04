ee8d1dfbba24805887ed2392a59f720d59ff875d
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-03-04 10:32:00 +0200

    Update openldap.rb

--- Formula/openldap.rb
+++ Formula/openldap.rb
@@ -1,15 +1,15 @@
 class Openldap < Formula
   desc "Open source suite of directory software"
   homepage "https://www.openldap.org/software/"
-  url "http://10.10.4.242:8081/openldap-2.4.48.tgz"
+  url "http://10.10.4.242:8081/openldap-2.4.49.tgz"
   sha256 "d9523ffcab5cd14b709fcf3cb4d04e8bc76bb8970113255f372bc74954c6074d"
   revision 1
 
   bottle do
     root_url "http://10.10.4.242:8081/bottles"
-    sha256 "07e1f0e3ec1a02340a82259e1ace713cfb362126404575032713174935f4140e" => :mojave
-    sha256 "8901626fc45d76940dec5e516b23d81c9970f4a4a94650bdad60228d604c1b4a" => :high_sierra
-    sha256 "644e0ba997bdc81cddd8906cb7eee96bd44ed7e6ad4a586db6313f13e251b0e2" => :catalina
+    sha256 "26efb37f53ae1a3815213adf9ab815c66b7197a3d212f994dd4e5b4140cf3e4e" => :catalina
+    sha256 "b1572831c24149585e73eaafbc08dc66d9edfca69f72cdeaaabf1e9c4b2ddda1" => :mojave
+    sha256 "eb9c40191d88785247026a699bd6fe0e2e9e669395d814d7d1be430843a92b05" => :high_sierra
   end
 
   keg_only :provided_by_macos
