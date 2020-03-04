4b8d4e33e8c6b1a6f8a749e8a74058ad92ebff52
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-03-04 17:48:31 +0200

    Create x265.rb

---
+++ Formula/x265.rb
@@ -0,0 +1,62 @@
+class X265 < Formula
+  desc "H.265/HEVC encoder"
+  homepage "https://bitbucket.org/multicoreware/x265"
+  url "http://10.10.4.242:8081/x265_3.3.tar.gz"
+  sha256 "f26e148ed1f4dfb33fd1eb3ff5e60e08078d1b2017e88bcbb045b3fb58300b9c"
+  head "https://bitbucket.org/multicoreware/x265", :using => :hg
+
+  bottle do
+    cellar :any
+    sha256 "81909d142c67d3228c55e6eae8819b9101d4256179322703df007f1ff8f4df18" => :catalina
+    sha256 "242d1f5a737a53054d071d36e57d3f86aa7f3387b5622b18c706ae6847476d22" => :mojave
+    sha256 "4e8eb694bd3775d6725cd486c9abe812ac82f353c06f4d294a278f4a3a96b916" => :high_sierra
+  end
+
+  depends_on "cmake" => :build
+  depends_on "nasm" => :build
+
+  def install
+    # Build based off the script at ./build/linux/multilib.sh
+    args = std_cmake_args + %w[
+      -DLINKED_10BIT=ON
+      -DLINKED_12BIT=ON
+      -DEXTRA_LINK_FLAGS=-L.
+      -DEXTRA_LIB=x265_main10.a;x265_main12.a
+    ]
+    high_bit_depth_args = std_cmake_args + %w[
+      -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF
+      -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
+    ]
+    (buildpath/"8bit").mkpath
+
+    mkdir "10bit" do
+      system "cmake", buildpath/"source", *high_bit_depth_args
+      system "make"
+      mv "libx265.a", buildpath/"8bit/libx265_main10.a"
+    end
+
+    mkdir "12bit" do
+      system "cmake", buildpath/"source", "-DMAIN12=ON", *high_bit_depth_args
+      system "make"
+      mv "libx265.a", buildpath/"8bit/libx265_main12.a"
+    end
+
+    cd "8bit" do
+      system "cmake", buildpath/"source", *args
+      system "make"
+      mv "libx265.a", "libx265_main.a"
+      system "libtool", "-static", "-o", "libx265.a", "libx265_main.a",
+                        "libx265_main10.a", "libx265_main12.a"
+      system "make", "install"
+    end
+  end
+
+  test do
+    yuv_path = testpath/"raw.yuv"
+    x265_path = testpath/"x265.265"
+    yuv_path.binwrite "\xCO\xFF\xEE" * 3200
+    system bin/"x265", "--input-res", "80x80", "--fps", "1", yuv_path, x265_path
+    header = "AAAAAUABDAH//w=="
+    assert_equal header.unpack("m"), [x265_path.read(10)]
+  end
+end
