cf8ec98e802812f22893a941c9c2e7a63413db35
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-03-04 14:53:35 +0200

    Update glib.rb

--- Formula/glib.rb
+++ Formula/glib.rb
@@ -1,14 +1,14 @@
 class Glib < Formula
   desc "Core application library for C"
   homepage "https://developer.gnome.org/glib/"
-  url "http://10.10.4.242:8081/glib-2.62.5.tar.xz"
-  sha256 "b8d1cdafa46658b63d7512efbe2cd21bd36cd7be83140e44930c47b79f82452e"
+  url "http://10.10.4.242:8081/glib-2.64.0.tar.xz"
+  sha256 "e5e514e47d169cdb4111c3ea4af0300e1b1a5f428a474d2d7ddadf38dd061280"
 
   bottle do
     root_url "http://10.10.4.242:8081/bottles"
-    sha256 "ffe825b5349fa644019af75ab82581fe312c689ab4c6add223ee119e1f35733d" => :catalina
-    sha256 "5c6ff19a133a3249fe5ae5996860eac5f1e7696a8d14429cfb3db6c0fa7c7635" => :mojave
-    sha256 "0dd0df3cb683714b7496537ccc8ea4f1f1b97301a36a58f2d9e8542922c389bc" => :high_sierra
+    sha256 "d8337688b45c724480859e94036aef581549d4fced5a782411abaa68adf60c50" => :catalina
+    sha256 "a70289637c08fdfcd4ee79bbd3ed9a8c519c645d73d70bf06f449e7fc2d6ab1d" => :mojave
+    sha256 "9e7e98084cf6f98c12da534c43f2901ad91e0cc5b342c15bf8b84072428a8652" => :high_sierra
   end
 
   # autoconf, automake and libtool can be removed when
