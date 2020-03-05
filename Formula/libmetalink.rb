a046e10bd1a71e0e5305fc1afd06c19cf3d97a23
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-03-05 09:57:17 +0200

    Update libmetalink.rb

--- Formula/libmetalink.rb
+++ Formula/libmetalink.rb
@@ -5,7 +5,7 @@ class Libmetalink < Formula
   sha256 "86312620c5b64c694b91f9cc355eabbd358fa92195b3e99517504076bf9fe33a"
 
   bottle do
-    cellar :any
+    root_url "http://10.10.4.242:8081/bottles"
     sha256 "a18e6199b4d775a32693aa38b80551d547eb1de58c5d7c788f706276ce296d5e" => :catalina
     sha256 "461d42fab1dd15b7df8144ef814a3b589c5be9177728b522ea73e6c52bec660f" => :mojave
     sha256 "037c9d82da0a2c77031515cc8618b28552c526e559b3d9cebb0f054cd66204e3" => :high_sierra
