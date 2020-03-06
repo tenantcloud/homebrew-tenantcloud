45be685443134c7e00596d133641364f8b47a173
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-03-06 23:36:50 +0200

    Update jpeg.rb

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -6,6 +6,7 @@ class Jpeg < Formula
 
   bottle do
     root_url "http://10.10.4.242:8081/bottles"
+    cellar :any
     sha256 "8f7b82a952fb3937889c7f22da1403e5338cd320495917eb26b0c5b2e614791c" => :catalina
     sha256 "b931e7725c83275c56f962b51b83c10f31a01ac8d823c6722edaf16d893970b1" => :mojave
     sha256 "64286932634fbe1e0d07eacda334d2f4967b20bce0737424df56ec5eaa34ccca" => :high_sierra
