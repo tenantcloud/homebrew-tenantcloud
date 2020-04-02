ce17f829d89a9c9cfbfb30a81ffc77ee31f53fd5
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-04-02 17:31:09 +0300

    Update openssl@1.1.rb

--- Formula/openssl@1.1.rb
+++ Formula/openssl@1.1.rb
@@ -1,15 +1,15 @@
 class OpensslAT11 < Formula
   desc "Cryptography and SSL/TLS Toolkit"
   homepage "https://openssl.org/"
-  url "http://10.10.4.242:8081/openssl-1.1.1d.tar.gz"
-  sha256 "1e3a91bc1f9dfce01af26026f856e064eab4c8ee0a8f457b5ae30b40b8b711f2"
+  url "http://10.10.4.242:8081/openssl-1.1.1f.tar.gz"
+  sha256 "186c6bfe6ecfba7a5b48c47f8a1673d0f3b0e5ba2e25602dd23b629975da3f35"
   version_scheme 1
 
   bottle do
     root_url "http://10.10.4.242:8081/bottles"
-    sha256 "104ef018b7bb8fcc49f57e5a60359a28a02d480d85a959e6141394b0571cbb28" => :mojave
-    sha256 "c7681ee40cb3680cd9fafcdb092bde153b9d4903907d67858baa5f19025f927b" => :high_sierra
-    sha256 "d7f992ebfd78f80828051f6dc6a1a99aed405f86b0f39ea651fd0afeadd1b0f4" => :catalina
+    sha256 "724cd97c269952cdc28e24798e350fcf520a32c5985aeb26053ce006a09d8179" => :catalina
+    sha256 "25ab844d2f14fc85c7f52958b4b89bdd2965bbd9c557445829eff6473f238744" => :mojave
+    sha256 "27f26e2442222ac0565193fe0b86d8719559d776bcdd070d6113c16bb13accf6" => :high_sierra
   end
 
   keg_only :provided_by_macos,
