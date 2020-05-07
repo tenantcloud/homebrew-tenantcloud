3e8fbf90ec5fc7ec2dd1aec940a807ae0e89a0a5
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-05-07 18:18:44 +0300

    Update gettext.rb

--- Formula/gettext.rb
+++ Formula/gettext.rb
@@ -1,15 +1,15 @@
 class Gettext < Formula
   desc "GNU internationalization (i18n) and localization (l10n) library"
   homepage "https://www.gnu.org/software/gettext/"
-  url "http://10.10.4.242:8081/gettext-0.20.1.tar.xz"
-#  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.19.8.1.tar.xz"
-  sha256 "53f02fbbec9e798b0faaf7c73272f83608e835c6288dd58be6c9bb54624a3800"
+  url "http://10.10.4.242:8081/gettext-0.20.2.tar.xz"
+  sha256 "b22b818e644c37f6e3d1643a1943c32c3a9bff726d601e53047d2682019ceaba"
+  revision 1
 
   bottle do
     root_url "http://10.10.4.242:8081/bottles"
-    sha256 "afc6a6120632b98d58b11fab82ae5e081206b89684dd948abf2d29caeb813ffd" => :mojave
-    sha256 "99d2dbd4c9ebfe9bf2a64bd99f3a695a18635f0d9110eaff34bab8022abef6a8" => :high_sierra
-    sha256 "6354adfc22f1d78b1a671862db906ff1e55482be9c58a6ee6a43245f8895fef2" => :catalina
+    sha256 "71f4ded03e8258b5e6896eebb00d26ed48307fbebece1a884b17ca3fb40e3121" => :catalina
+    sha256 "52067198cab528f05fdc0b06f7b9711f7614f60a7361f1e764c4f46d3342ff22" => :mojave
+    sha256 "4a999c75dcc53cbc711e3ac6545db69ab3aeca6c29c1cb6b21c353f237342457" => :high_sierra
   end
 
   keg_only :shadowed_by_macos,
