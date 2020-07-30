f1c0f3a65bcbd24ae5ed88f59bcf130904380f05
odebretseni <50202839+odebretseni@users.noreply.github.com>
2020-07-30 13:22:33 +0300

    Update gdbm.rb

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -4,13 +4,16 @@ class Gdbm < Formula
   url "http://10.10.4.242:8081/gdbm-1.18.1.tar.gz"
 #  mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.18.1.tar.gz"
   sha256 "86e613527e5dba544e73208f42b78b7c022d4fa5a6d5498bf18c8d6f745b91dc"
+  license "GPL-3.0"
+  revision 1
 
   bottle do
     root_url "http://10.10.4.242:8081/bottles"
     cellar :any
     rebuild 1
-    sha256 "2168d58856917ca996d12dedaa930643529c66046103fe55018afc51f2bc1fcb" => :mojave
-    sha256 "ac688d571f9c00e09670440d67d2869a34dab0fb897ba0b183ed84fceffdbc9c" => :high_sierra
+    sha256 "f7b5ab7363961fa6defcb66b4ffdf5365264fcb97d35bc413e754f173a3b1912" => :catalina
+    sha256 "0f65874bcd50d31aaf8b2e6c8ef414cb65a8d8b9eb6d1fa4ef179c6e0a94983c" => :mojave
+    sha256 "4a644af2fcc2781c3a161209deff7b62d760058bc1bac7c4f91a5ce5738f0798" => :high_sierra
   end
 
   # Use --without-readline because readline detection is broken in 1.13
